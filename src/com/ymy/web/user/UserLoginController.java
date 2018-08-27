cf39868aaffc30085f3da3e9875a59de19efc5e8
Administrator <847207960@qq.com>
2018-08-27 16:33:53 +0800

    移动端登陆接口

--- src/com/ymy/web/user/UserLoginController.java
+++ src/com/ymy/web/user/UserLoginController.java
@@ -109,7 +109,80 @@ public class UserLoginController {
 		}
 		return "redirect:/manage/menu/welcome";
 	}
-	
+
+    @RequestMapping(value="/vue",method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,Object> loginVue(String telephone, String password, HttpServletRequest request, String issave, Model model,
+                        HttpServletResponse response) {
+        Map<String,Object> result=new HashMap<>();
+        User user = userService.findByName(telephone);
+        if (user != null && user.getIsdel() == 0) {
+            boolean flag = userService.decryptPassword(user, password);
+            if (!flag) {
+                result.put("result","-1");
+                result.put("msg","您输入的密码和账户不匹配,请重新输入!");
+                return result;
+            }
+        } else {
+            result.put("result","0");
+            result.put("msg","您输入的账号不存在,请重新输入!");
+            return result;
+        }
+        if (issave != null) {
+            Cookie ck = new Cookie("user", user.getId().toString());
+            ck.setMaxAge(999999999);
+            response.addCookie(ck);
+        }
+
+        String loginIp = request.getRemoteAddr();
+        logger.error("用户" + user.getName() + "登陆的ip为    : " + loginIp);
+        System.out.println("登陆的ip为    : " + loginIp);
+
+        if (loginIp.equals("0:0:0:0:0:0:0:1")) {
+            loginIp = "127.0.0.1";
+        }
+
+        //
+        // 登陆成功 user放入缓存
+        HttpSession session = request.getSession();
+        // session.setMaxInactiveInterval(60*5);
+        session.setAttribute("user", user);
+        List<Map<Resource, List<Resource>>> mapList = new ArrayList<Map<Resource, List<Resource>>>();
+        List<Resource> resList = resourceService.findByRoleId(user.getRole().getId());
+        if (resList != null && resList.size() > 0) {
+            List<Long> idList = new ArrayList<Long>();
+            for (Resource res : resList) {
+                if (res != null) {
+                    Resource pRes = res.getResource();
+                    if (pRes == null) {
+                        idList.add(res.getId());
+                    } else {
+                        if (idList.contains(pRes.getId())) {
+                            continue;
+                        } else {
+                            idList.add(pRes.getId());
+                        }
+                    }
+                }
+            }
+            List<Long> sortedList = resourceService.getSortedList(idList);
+            // System.out.println(sortedList);
+            for (Long pid : sortedList) {
+                Resource pRes = new Resource();
+                pRes = resourceService.get(pid);
+                // System.out.println(pRes.getName());
+                List<Resource> subResList = resourceService.findSubResources(pid, user.getRole().getId());
+                Map<Resource, List<Resource>> map = new HashMap<Resource, List<Resource>>();
+                map.put(pRes, subResList);
+                mapList.add(map);
+            }
+            // request.getSession().setAttribute("access", resList);
+            request.getSession().setAttribute("menuList", mapList);
+        }
+        result.put("result","1");
+        result.put("msg","登陆成功！");
+        return result;
+    }
 	
 	@RequestMapping(value="editpwd")
 	@ResponseBody
